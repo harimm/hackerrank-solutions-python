0352a637cec0a8ef196b023bb86c9aabd5303028
Harry M <harimm@users.noreply.github.com>
2021-08-04 10:42:33 +0530

    Update README and solution files

--- solutions/python/halloween-sale.py
+++ solutions/python/halloween-sale.py
@@ -1,17 +1,20 @@
 #!/bin/python3
 
-def howManyGames(p, d, m, s):
+def how_many_games(p, d, m, s):
     count = 0
+    # Initialize current cost and starting cost to value of first item
     temp_cost = p
     current_cost = p
     # Return the number of games you can buy
     while temp_cost <= s:
         count += 1
+        # Find the next cost by checking the maximum of newly calculated value and predefined minimum
         current_cost = max(current_cost - d, m)
+        # Add the next cost to temporary total cost
         temp_cost += current_cost
     return count
 
 
 p, d, m, s = map(int, input().strip().split(' '))
-answer = howManyGames(p, d, m, s)
+answer = how_many_games(p, d, m, s)
 print(answer)
